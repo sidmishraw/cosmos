80521e2a875121def4e54025cb6dfe48d5d945fd
Sidharth Mishra <sidmishraw@gmail.com>
2017-11-30 20:07:14 -0800

    fixing the file rename issue

--- code/unclassified/biggest_of_n_numbers/biggest_of_n_numbers2.cpp
+++ code/unclassified/biggest_of_n_numbers/biggest_of_n_numbers2.cpp
@@ -1,18 +1,19 @@
-// compile with c++11 standard
-
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
-int main()
-{
-  // initialising amd constructing vector array
-  vector<int> vec = {10,5,20,15,-1};
 
-  // sorting the vector
-  sort(vec.begin(), vec.end());
-
-  // printing the last element
-  cout<<"biggest number is : "<<vec[vec.size() - 1]
+int main() {
+  int n, max, tmp;
+  cout << "Enter numbers of elements : ";
+  cin >> n;
+  cout << "Enter numbers\n";
+  cin >> tmp;
+  max = tmp;
+  for (int i = 0; i < n - 1; i++) {
+    cin >> tmp;
+    if (max < tmp) {
+      max = tmp;
+    }
+  }
+  cout << "Maximum is " << max << "\n";
   return 0;
 }
